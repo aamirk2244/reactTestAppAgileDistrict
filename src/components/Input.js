81ef37ec43ea9f2c049395e403ebf3d3a739dfc5
aamirk2244 <aamir@agiledistrict.com>
2023-10-05 19:16:20 +0500

    Added useRef and Error Boundaries

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,12 @@
-function Input(props) {
-  const { type, placeholder, value, className, onChange } = props;
+function Input(props, references) {
+  const { type, placeholder, value, className, onChange, inputRef } = props;
 
   return (
     <input
       type={type}
       placeholder={placeholder}
       value={value}
+      ref={inputRef}
       onChange={onChange}
       className={className}
     />
